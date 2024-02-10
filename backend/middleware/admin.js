9fa7edeffadb1bec10a0aac8fa05e5e69419c1ee
e41c <ericgrigor@gmail.com>
2024-02-10 14:31:26 -0500

    updated parameters based on meeting

---
+++ backend/middleware/admin.js
@@ -0,0 +1,11 @@
+// middleware/admin.js
+const isAdmin = (req, res, next) => {
+    if (req.user && req.user.role === 'admin') {
+      next();
+    } else {
+      res.status(403).json({ message: 'Unauthorized' });
+    }
+  };
+  
+  module.exports = isAdmin;
+  
--- backend/models/Student.js
+++ backend/models/Student.js
@@ -1,24 +1,27 @@
 // models/Student.js
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const studentSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true }
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const studentSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phoneNumber: { type: String },
+  bio: { type: String },
   // Add more fields as needed
 });
 
 // Hash password before saving user
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-      return next();
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  });
-  
+studentSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 const Student = mongoose.model('Student', studentSchema);
 
