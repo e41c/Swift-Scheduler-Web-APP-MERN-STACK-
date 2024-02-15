d60210326242f8c46e3d563ac2cf8031db0f9ede
Yeganeh <vnus.daneshparvar@gmail.com>
2024-02-15 13:08:09 -0500

    login, landing page added

---
+++ frontend/src/components/LandingPage.jsx
@@ -0,0 +1,10 @@
+import background from '../assets/landingp.jpg'
+export default function LandingPage() {
+  return (
+    <div className='min-h-screen bg-cover bg-center bg-no-repeat' style={{backgroundImage: `url(${background})`}}>
+      {/* <img src={background} alt="landing page" className="w-full h-screen object-cover" /> */}
+      <h1></h1>
+      
+    </div>
+  )
+}
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+import postcss from './postcss.config.js'
 export default defineConfig({
   plugins: [react()],
+  css:{
+    postcss
+
+  }
 })
