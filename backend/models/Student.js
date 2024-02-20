ae91d61f82b04cb999de8ee87f74f0ff8cb4de72
Yeganeh <vnus.daneshparvar@gmail.com>
2024-02-20 18:02:10 -0500

    trying to resolve conflicts

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -14,14 +14,14 @@ const studentSchema = new mongoose.Schema({
 });
 
 // Hash password before saving user
-// studentSchema.pre('save', async function (next) {
-//   if (!this.isModified('password')) {
-//     return next();
-//   }
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-//   next();
-// });
+studentSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 const Student = mongoose.model('Student', studentSchema);
 
