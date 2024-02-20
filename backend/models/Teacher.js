2f3aa84f91e7c39df8f5c7e107cacd7cb79faba0
Yeganeh <vnus.daneshparvar@gmail.com>
2024-02-20 15:55:22 -0500

    added Chris's login fix

--- backend/models/Teacher.js
+++ backend/models/Teacher.js
@@ -13,18 +13,18 @@ const teacherSchema = new mongoose.Schema({
 });
 
 // Hash password before saving teacher
-teacherSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
+// teacherSchema.pre('save', async function (next) {
+//   if (!this.isModified('password')) {
+//     return next();
+//   }
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//     next();
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 const Teacher = mongoose.model('Teacher', teacherSchema);
 
