16e8d0615a0b865caf3f140a222df5c5e1194cda
Yeganeh <vnus.daneshparvar@gmail.com>
2024-02-20 15:42:06 -0500

    working on login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -14,10 +14,13 @@ export default function Login() {
       [name]: type === 'checkbox' ? checked : value,
     }));
   };
+  const onLogin = async (e) => {
+
+  }
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <form onSubmit={onLogin} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
 
 
         <div className="mb-4">
