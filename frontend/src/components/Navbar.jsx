4945de792021a35d9c715805bb25120f6cbc7964
Yeganeh <vnus.daneshparvar@gmail.com>
2024-02-21 18:51:39 -0500

    responsive navbar and logout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,50 @@
-import { Link } from 'react-router-dom'
-import { useAuth } from '../AuthContext'
+import { Link } from 'react-router-dom';
+import { useAuth } from '../AuthContext';
+
 export default function Navbar() {
-  const {isAuthenticated, logout, auth} = useAuth();
+  const { isAuthenticated, logout, auth } = useAuth();
+  const navItems = [
+    { name: "greet", link: null, auth: true, text: `Hello, ${auth?.user}`, onclick: null },
+    { name: "logout", link: null, auth: true, text: "Logout", onclick: logout },
+    { name: "profile", link: "/profile", auth: true, text: "Profile", onclick: null },
+    { name: "login", link: "/login", auth: false, text: "Login", onclick: null },
+    { name: "register", link: "/register", auth: false, text: "Join Our Team", onclick: null },
+  ];
+
   return (
     <nav className="fixed w-full left-0 top-0 z-50 bg-secondary bg-opacity-50 shadow border-b-2 border-white">
       <ul className='flex justify-between items-center p-4 mr-6'>
-        {isAuthenticated? <li className='nav-link'>Welcome {auth.user}, {auth.role}</li> : null}
-        <li className='nav-link'><Link to="/">Home</Link></li>
-        <li className='nav-link'><Link to="/about">ABOUT US</Link></li>
-        <li className='nav-link'><Link to="/register" state={{role: "staff"}}>JOIN OUR TEAM!</Link></li>
-        <li className='nav-link'><Link to="/login">ALREADY A MEMBER? LOGIN!</Link></li>
+        {navItems.map((item, index) => {
+          // Render the item if authentication matches the item's auth property
+          if (isAuthenticated && item.auth) {
+            return (
+              <li key={index} className='nav-link'>
+                {item.link ? 
+                  <Link to={item.link}>{item.text}</Link> :
+                  <button onClick={item.onclick}>{item.text}</button>
+                }
+              </li>
+            )
+          }else if(!item.auth && !isAuthenticated){
+            return(
+              <li key={index} className='nav-link'>
+                {item.link ? 
+                  <Link to={item.link}>{item.text}</Link> :
+                  <button onClick={item.onclick}>{item.text}</button>
+                }
+              </li>
+            )
+          }
+          
+          //return null; // Return null for items that should not be displayed
+        })}
+        <li className='nav-link'>
+          <Link to='/'>Home</Link>
+        </li>
+        <li className='nav-link'>
+          <Link to='/about'>About Us</Link>
+        </li>
       </ul>
-
-
     </nav>
   )
 }
