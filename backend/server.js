1dfba642f5ea00f951173971d03507873dd36800
Yeganeh <vnus.daneshparvar@gmail.com>
2024-03-28 23:57:55 -0400

    switching to atlas cluster

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,9 @@ app.use('/classrooms', classroomRouter);
 app.use('/classes', classRouter); // This line is updated
 
 const PORT = process.env.PORT || 3000;
-mongoose.connect('mongodb://admin:password@localhost:27017')
+'mongodb+srv://rootadmin:m5NvavxMIOPodOKz@clusterrl.wd5fhyo.mongodb.net/Swift-Scheduler'
+//'mongodb://admin:password@localhost:27017'
+mongoose.connect('mongodb+srv://rootadmin:m5NvavxMIOPodOKz@clusterrl.wd5fhyo.mongodb.net/Swift-Scheduler')
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
 
