b81cdfc23db64a059e247000286924040ba1208e
e41c <ericgrigor@gmail.com>
2024-03-31 00:21:24 -0400

    adding env to frontend for localhost and render API to backend, and updated api.js to work with env

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,19 +1,22 @@
+// api.js
 import axios from 'axios';
-// import { useAuth } from './AuthContext';
+
+const BASE_URL = import.meta.env.DEV ? import.meta.env.VITE_LOCAL_API_URL : import.meta.env.VITE_RENDER_API_URL;
+
 export const FetchClasses = async (token) => {
-    
     if (!token) {
-        return;
+        return Promise.reject(new Error('No token provided'));
     }
-    else {
-        return axios.get('/classes', {
+
+    try {
+        const response = await axios.get(`${BASE_URL}/classes`, {
             headers: {
                 Authorization: `Bearer ${token}`
             }
-        })
-            .then(response => response.data)
-            .catch(error => error);
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching classes:', error);
+        throw error;
     }
-    
-
-}
+};
