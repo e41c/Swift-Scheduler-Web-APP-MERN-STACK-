4df87d0bd6a464028991c57644e082360042631c
Chris Tri <101335147@georgebrown.ca>
2024-04-10 18:22:07 -0400

    authcontext, navbar admin changes

--- frontend/src/components/adminView/ClassesAdmin.jsx
+++ frontend/src/components/adminView/ClassesAdmin.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ClassesAdmin() {
   return (
     <div className='text-white'>
-      class
+      
     </div>
   )
 }
--- frontend/src/components/adminView/StudentAdmin.jsx
+++ frontend/src/components/adminView/StudentAdmin.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import {useState} from 'react'
 
 function StudentAdmin() {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] =  useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+
+  
+
+
   return (
-    <div>
-      student
+    <div className='size-full flex flex-col items-center text-white '>
+      <div className='m-2 p-4 rounded shadow-lg bg-black
+      sm:size-1/2'>
+        <h3>Register</h3>
+        <form>
+          <label htmlFor="firstName">First Name: </label>
+          <input type="text" id="firstName" name="firstName" required /><br />
+
+          <label htmlFor="lastName">Last Name: </label>
+          <input type="text" id="lastName" name="lastName" required /><br />
+
+          <label  htmlFor="email">Email: </label>
+          <input type="email" id="email" name="email" required /><br />
+
+          <label htmlFor='password' >password</label>
+          <input type='password' name='password'></input> <br/>
+
+          <label htmlFor='phoneNumber'>Phone Number: </label>
+          <input type='tel' name='phoneNumber' pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required /><br />
+
+          <button className='bg-indigo-600 rounded-md p-2' type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   )
 }
