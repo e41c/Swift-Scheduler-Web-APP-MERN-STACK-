d5c5e6a7c143e3eb81ee94e21ef773d4e0e09929
e41c <ericgrigor@gmail.com>
2024-04-12 02:19:52 -0400

    updated expected URL to ven

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,8 @@ require('dotenv').config();
 
 app.use(bodyParser.json());
 app.use(cors({
-    // origin: 'https://swift-scheduler-frontend.vercel.app',
-    origin: 'https://capstone-ii-group26.vercel.app/',
+    origin: 'https://swift-scheduler-frontend.vercel.app',
+    // origin: 'https://capstone-ii-group26.vercel.app/',
     credentials: true
 
 }));
