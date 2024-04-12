f2f9e79114ee97692e0f4f8e653fafec253c889e
Chris Tri <101335147@georgebrown.ca>
2024-04-12 11:43:46 -0400

    vite.config change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,13 +14,13 @@ export default defineConfig({
     proxy:{
       '/auth': {
         // target: 'http://localhost:3000',
-        target: 'https://capstone-ii-group26-1.onrender.com',
+        target: 'https://capstone-ii-group26.onrender.com',
         changeOrigin: true,
         secure: false,
     },
     '/api': {
       // target: 'http://localhost:3000',
-      target: 'https://capstone-ii-group26-1.onrender.com',
+      target: 'https://capstone-ii-group26.onrender.com',
       changeOrigin: true,
       secure: false,
       rewrite: (path) => path.replace(/^\/api/, '') // remove the /api prefix before forwarding
